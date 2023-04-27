c65b385896d948ec052f4627c42a73850692a482
ethanwang1213 <ethansage1213@gmail.com>
2023-04-28 03:46:57 +0900

    feat: pages to test profile

---
+++ apps/journal/pages/test/profile0/index.tsx
@@ -0,0 +1,8 @@
+import ProfilePage0 from "@/components/pages/ProfilePage/ProfilePage0/ProfilePage0";
+import { NextPage } from "next";
+
+const TestProfile0: NextPage = () => {
+  return <ProfilePage0 />;
+};
+
+export default TestProfile0;
---
+++ apps/journal/pages/test/profile1/index.tsx
@@ -0,0 +1,8 @@
+import ProfilePage1 from "@/components/pages/ProfilePage/ProfilePage1/ProfilePage1";
+import { NextPage } from "next";
+
+const TestProfile0: NextPage = () => {
+  return <ProfilePage1 />;
+};
+
+export default TestProfile0;
