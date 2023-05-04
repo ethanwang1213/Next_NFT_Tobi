6273caa1f5e86310cb9902f242c0bdc4ec78c2fe
ethanwang1213 <ethansage1213@gmail.com>
2023-05-05 00:03:12 +0900

    feat: set daisyui theme

--- apps/journal/tailwind.config.js
+++ apps/journal/tailwind.config.js
@@ -1,1 +1,24 @@
-module.exports = require("../../packages/tailwind-config/tailwind.config.js");
+/** @type {import('tailwindcss').Config} */
+const general = require("../../packages/tailwind-config/tailwind.config.js");
+const config = {
+  ...general,
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#BE6105",
+          secondary: "#F8F0D8",
+          accent: "#137BB1",
+          neutral: "#7D5337",
+          "base-100": "#FCF9F1",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FACC15",
+          error: "#E5574B",
+        },
+      },
+    ],
+  },
+};
+
+module.exports = config;
