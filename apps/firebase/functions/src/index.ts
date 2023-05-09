d356f4421f4a068322781a1fa2c3e6f286100b3d
ethanwang1213 <ethansage1213@gmail.com>
2023-05-09 15:12:48 +0900

    add discordOAuth

--- apps/firebase/functions/src/index.ts
+++ apps/firebase/functions/src/index.ts
@@ -4,6 +4,7 @@ import * as sendgrid from "@sendgrid/mail";
 import * as voucher from "voucher-code-generator";
 import {initializeApp, applicationDefault} from "firebase-admin/app";
 import {firestore} from "firebase-admin";
+import fetch from "node-fetch";
 
 // initializeApp() is not needed in Cloud Functions for Firebase
 initializeApp({
@@ -104,4 +105,41 @@ export const pubsubHelper = functions.https.onRequest(async (request, response) 
   response.status(201).send(
       {success: "Published to pubsub test-topic -- message ID: ", messageID}
   );
+});
+
+export const discordOAuth = functions.https.onRequest(async (request, response) => {
+  const code = request.query.code;
+  if (!code || typeof code !== "string") {
+    response.status(500).send("Invalid parameter of code").end();
+    return;
+  }
+  const params = new URLSearchParams();
+  params.append("client_id", process.env.DISCORD_OAUTH_CLIENT_ID || "discord_oauth_client_id");
+  params.append("client_secret", process.env.DISCORD_OAUTH_CLIENT_SECRET || "discord_oauth_client_secret");
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", process.env.DISCORD_OAUTH_CLIENT_REDIRECT_URI || "http://localhost:3000/authed");
+  params.append("scope", "identify");
+  const tokenResponse: any = await (await fetch("https://discordapp.com/api/oauth2/token", {
+    method: "POST",
+    body: params,
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+  })).json();
+  if (tokenResponse.error) {
+    response.status(500).send( "Can't get token").end();
+    return;
+  }
+  const accessToken = tokenResponse.access_token;
+  const userdata: any = await (await fetch("https://discordapp.com/api/users/@me", {
+    headers: {
+      "Authorization": `Bearer ${accessToken}`,
+    },
+  })).json();
+  if (userdata.code == 0) {
+    response.status(500).send("Can't get userdata").end();
+    return;
+  }
+  response.status(200).send(userdata).end();
 });
