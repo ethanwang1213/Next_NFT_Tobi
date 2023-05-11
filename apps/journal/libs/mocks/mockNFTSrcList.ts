a24872484a1638d5eea473de928002bd790cc0d0
ethanwang1213 <ethansage1213@gmail.com>
2023-05-12 03:26:50 +0900

    fix: mock path

--- apps/journal/libs/mocks/mockNFTSrcList.ts
+++ apps/journal/libs/mocks/mockNFTSrcList.ts
@@ -3,146 +3,146 @@ import { NFTSrc } from "../../types/NFTSrc";
 export const mockNFTSrcList: NFTSrc[] = [
   {
     id: 0,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 1,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 2,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 3,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 4,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 5,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 6,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 7,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 8,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 9,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 10,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 11,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 12,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 13,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 14,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 15,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 16,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 17,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 18,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 19,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 20,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 21,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 22,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 23,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 24,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 25,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 26,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 27,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 28,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 29,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 30,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 31,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 32,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 33,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 34,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
   {
     id: 35,
-    src: "/mock/images/neko.png",
+    src: "/mocks/images/neko.png",
   },
 ];
