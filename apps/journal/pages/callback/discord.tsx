1f4ae80860972ff7a90faf827a68b20975167537
ethanwang1213 <ethansage1213@gmail.com>
2023-05-16 14:42:00 +0900

    implement discord.tsx before firestore

--- apps/journal/pages/callback/discord.tsx
+++ apps/journal/pages/callback/discord.tsx
@@ -1,6 +1,40 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 const Discord = () => {
+
+  const router = useRouter();
+  const query = router.query;
+
+  useEffect((() => {
+    const getUserdata = async () => {
+      const response = await fetch(process.env.DISCORD_OAUTH_API_URL);
+      if (response.status == 200) {
+        return await response.json();
+      } else {
+        return null;
+      }
+    };
+
+    if (router.isReady) {
+      const code = query.code;
+      if (!code) {
+        router.replace("/");
+        return;
+      }
+      const userdata = getUserdata();
+      if (userdata) {
+        // firestore
+      } else {
+        router.replace("/");
+      }
+    }
+  }), [query, router]);
+
   return (
-    <></>
+    <div className={"flex justify-center items-center"}>
+      <span>Loading...</span>
+    </div>
   );
 };
 
