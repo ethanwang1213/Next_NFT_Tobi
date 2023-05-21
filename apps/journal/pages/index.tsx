64d1d50e32ed47c8a691aedf3c71d2a65078bd6f
knagato <nagato.kasaki@gmail.com>
2023-05-21 11:26:49 +0900

    add discord param to user type

--- apps/journal/pages/index.tsx
+++ apps/journal/pages/index.tsx
@@ -1,8 +1,12 @@
 import Mobile from "../components/book/Mobile";
 import Pc from "../components/book/Pc";
 import Image from "next/image";
+import { useAuth } from "@/contexts/AuthProvider";
 
 const Index = () => {
+  const currentUser = useAuth();
+  console.log(currentUser);
+
   return (
     <>
       <Image src="/images/book/bg_journal.png" fill alt="bg_journal"></Image>
--- apps/journal/types/type.d.ts
+++ apps/journal/types/type.d.ts
@@ -31,6 +31,7 @@ export type User = {
     id: string;
     name: string;
     email: string;
+    discord?: string;
     icon: string;
     createdAt: number;
 };
