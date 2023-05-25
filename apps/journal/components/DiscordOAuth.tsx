4ac46a14d6ccdf9b821b1c17b5c77e7f6dad82fb
ethanwang1213 <ethansage1213@gmail.com>
2023-05-25 11:38:20 +0900

    :zap: invite community button

--- apps/journal/components/DiscordOAuth.tsx
+++ apps/journal/components/DiscordOAuth.tsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "@/contexts/AuthProvider";
+import { getAuth } from "@firebase/auth";
+
 const DiscordOAuth = () => {
+
+  const auth = getAuth();
+  const user = useAuth();
+  const [discordId, setDiscordId] = useState<string | null>();
+
+  useEffect(() => {
+    if (auth.currentUser) {
+      const discord = user.discord;
+      if (discord) {
+        setDiscordId(discord);
+      }
+    }
+  }, [auth.currentUser, user]);
+
   return (
-    <a href={process.env.DISCORD_OAUTH_URL}>
-      Discord 連携
-    </a>
+    <>
+      { discordId ? (
+        <a href={process.env.DISCORD_COMMUNITY_INVITE_URL}>
+          コミュニティに参加
+        </a>
+      ) : (
+        <a href={process.env.DISCORD_OAUTH_URL}>
+          Discord 連携
+        </a>
+      ) }
+    </>
   );
 };
 
