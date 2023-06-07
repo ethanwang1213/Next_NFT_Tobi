3d9060266b57fa43c5fe8de0a46a7aa4d9e1e381
knagato <nagato.kasaki@gmail.com>
2023-06-07 19:06:32 +0900

    add NFT_ADDRESS env

--- apps/firebase/functions/src/lib/constants.ts
+++ apps/firebase/functions/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const NEKO_NFT_ID = "A.e217638793f1e461.TobiraNeko";
+export const NEKO_NFT_ID = process.env.NEKO_NFT_ADDRESS || "A.5e9ccdb91ff7ad93.TobiraNeko"; // default for testnet
 export const TOPIC_NAMES = {
   ordersPaid: "shopify-orders-paid",
   ordersCreate: "shopify-orders-create",
