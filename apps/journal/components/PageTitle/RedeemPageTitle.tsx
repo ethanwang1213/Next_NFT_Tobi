231de18b392734258d396f731917002df1788100
ethanwang1213 <ethansage1213@gmail.com>
2023-06-09 01:43:20 +0900

    fix: rename redeem title

--- apps/journal/components/PageTitle/RedeemPageTitle.tsx
+++ apps/journal/components/PageTitle/RedeemPageTitle.tsx
@@ -13,8 +13,8 @@ const RedeemPageTitle: React.FC<Props> = ({ isShown }) => {
   return (
     <PageTitle
       isShown={isShown}
-      className="h-[18%] min-h-[18%] sm:h-[214px] sm:min-h-[214px] grid content-center"
-      title="シリアルコード"
+      className="h-[18%] min-h-[18%] sm:h-[214px] sm:min-h-[214px] grid content-center -mx-4"
+      title="Redeem Code"
     />
   );
 };
