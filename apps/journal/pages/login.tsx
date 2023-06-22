54b90d1e3c1712345538d77adef9c5c06770c435
ethanwang1213 <ethansage1213@gmail.com>
2023-06-22 14:05:23 +0900

    fix: modal sentences, style

--- apps/journal/pages/login.tsx
+++ apps/journal/pages/login.tsx
@@ -298,12 +298,25 @@ const Login = () => {
         />
       </div>
       <dialog id="loginModal" className="modal" ref={emailModalRef}>
-        <form method="dialog" className="modal-box">
-          <h3 className="font-bold text-lg">メールを送信しました</h3>
-          <p className="py-4">ログイン認証に必要なメールを送信しましたのでメール内のリンクをクリックしログインをしてください。<br />数分たっても届かない場合は再度お試しください。</p>
+        <form method="dialog" className="modal-box bg-secondary">
+          <h3 className="font-bold text-base sm:text-xl text-accent">
+            メールを送信しました
+          </h3>
+          <div className="text-xs sm:text-sm text-accent">
+            <p className="py-4">
+              ログイン認証のメールを記入されたアドレスへ送信しました。
+              <br />
+              メールをご確認いただき、記載されたURLからログインを完了させてください。
+            </p>
+            <p className="font-bold">メールが届かない場合</p>
+            <ul className="list-disc pl-6 mt-2">
+              <li>迷惑メールフォルダやフィルターの設定をご確認ください。</li>
+              <li>時間をあけてから、再度ログインをお試しください。</li>
+            </ul>
+          </div>
           <div className="modal-action">
             {/* if there is a button in form, it will close the modal */}
-            <button className="btn">閉じる</button>
+            <button className="btn btn-sm sm:btn-md text-xs sm:text-base btn-outline btn-accent">閉じる</button>
           </div>
         </form>
       </dialog>
