395b734d244b361f0ac1d56cb3e10446088ae4ac
knagato <nagato.kasaki@gmail.com>
2023-06-25 01:29:13 +0900

    add function for get nft metadata

--- apps/firebase/functions/src/index.ts
+++ apps/firebase/functions/src/index.ts
@@ -2,6 +2,7 @@ import {initializeApp, applicationDefault} from "firebase-admin/app";
 import {background} from "./shopifyApi";
 import {discordOAuth} from "./discord";
 import {discordUserNfts} from "./discordUserNfts";
+import {metadata} from "./tobiraneko";
 
 // initializeApp() is not needed in Cloud Functions for Firebase
 initializeApp({
@@ -13,6 +14,7 @@ exports.journalNfts = require("./journalNfts");
 exports.background = background;
 exports.discordOAuth = discordOAuth;
 exports.discordUserNfts = discordUserNfts;
+exports.tobiraneko = metadata;
 
 if (process.env.PUBSUB_EMULATOR_HOST) {
   exports.devtool = require("./devtool");
