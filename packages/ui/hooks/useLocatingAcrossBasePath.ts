8ea0b24c10d0811527288cf1f0e93a5808505c71
ethanwang1213 <ethansage1213@gmail.com>
2023-07-21 13:32:29 +0900

    fix: use router.push to lower path than basePath

--- packages/ui/hooks/useLocatingAcrossBasePath.ts
+++ packages/ui/hooks/useLocatingAcrossBasePath.ts
@@ -9,10 +9,19 @@ export const useLocatingAcrossBasePath = () => {
 
   const pushLocation = async (link: string) => {
     if (!router.basePath) {
+      // basePathが""の場合
       await router.push(link);
     } else {
-      console.log(`${window.location.host}${link}`);
-      window.location.href = `https://${window.location.host}${link}`;
+      // basePathが""以外の場合
+      if (link.startsWith(router.basePath)) {
+        // 遷移先がbasePath以下の階層の場合
+        // basePathを除去して遷移
+        await router.push(link.replace(router.basePath, ""));
+      } else {
+        // 遷移先がbasePathより上の階層の場合
+        // basePathを超えてrouter.pushできなかったので、window.location.hrefによって遷移
+        window.location.href = `https://${window.location.host}${link}`;
+      }
     }
   };
 
