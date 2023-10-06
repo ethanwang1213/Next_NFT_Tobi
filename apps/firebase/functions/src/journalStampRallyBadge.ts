bb0691a71d244557f1027a5d2efa987fd0d1788b
ethanwang1213 <ethansage1213@gmail.com>
2023-10-06 14:21:11 +0000

    feature: write mint status

--- apps/firebase/functions/src/journalStampRallyBadge.ts
+++ apps/firebase/functions/src/journalStampRallyBadge.ts
@@ -2,6 +2,8 @@ import * as functions from "firebase-functions";
 import { firestore } from "firebase-admin";
 import { REGION, STAMP_RALLY_KEYWORD } from "./lib/constants";
 
+type StampRallyMintStatusType = "NOTHING" | "IN_PROGRESS" | "DONE";
+
 /**
  * ログイン状態で、正しいスタンプラリーの合言葉が渡されていれば、
  * 記念のバッジNFTをmintする
@@ -20,11 +22,22 @@ exports.checkReward = functions.region(REGION).https.onCall(async (data, context
   if (!email) {
     throw new functions.https.HttpsError("not-found", "The user doesn't have email.");
   }
+  // 初めてのリクエストであるかチェック
+  const stampRallyMintStatus: StampRallyMintStatusType = user?.stampRallyMintStatus;
+  if (stampRallyMintStatus === "IN_PROGRESS" || stampRallyMintStatus === "DONE") {
+    throw new functions.https.HttpsError("already-exists", "This request has already submitted.")
+  }
   // 合言葉のチェック
   if (data.keyword !== STAMP_RALLY_KEYWORD) {
     throw new functions.https.HttpsError("invalid-argument", "The function must be called with correct keyword.")
   }
   // mint処理
+
   // mint待機状態へ
+  await firestore().collection("users").doc(context.auth.uid).set({
+    stampRallyMintStatus: "IN_PROGRESS",
+  }, {
+    merge: true,
+  })
   return "正解！";
 });
