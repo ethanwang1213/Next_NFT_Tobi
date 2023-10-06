d9643d36d2eb90c87fe4d07bd2ff5c5b619e75ba
ethanwang1213 <ethansage1213@gmail.com>
2023-10-06 17:43:25 +0000

    refactor: divide journal types and others

--- apps/journal/types/type.d.ts
+++ apps/journal/types/type.d.ts
@@ -1,7 +1,7 @@
 import { MetaMaskInpageProvider } from "@metamask/providers";
 import { Dispatch, ReactElement } from "react";
 import { Timestamp } from "firebase/firestore/lite";
-import { StampRallyRewardFormType, StampRallyMintStatusType } from "types";
+import { StampRallyRewardFormType, StampRallyMintStatusType } from "types/journal-types";
 
 export declare global {
   interface Window {
