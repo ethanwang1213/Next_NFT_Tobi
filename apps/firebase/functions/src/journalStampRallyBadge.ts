b9735e940a96f3a9c1078018db35c6ad0152ed76
ethanwang1213 <ethansage1213@gmail.com>
2023-10-08 20:29:34 +0000

    feat: handle multi stamps on function

--- apps/firebase/functions/src/journalStampRallyBadge.ts
+++ apps/firebase/functions/src/journalStampRallyBadge.ts
@@ -1,12 +1,14 @@
 import * as functions from "firebase-functions";
 import { firestore } from "firebase-admin";
-import { REGION, STAMP_RALLY_KEYWORD } from "./lib/constants";
-
-type StampRallyMintStatusType = "NOTHING" | "IN_PROGRESS" | "DONE";
+import { REGION, TPF2023_STAMP_RALLY_KEYWORDS } from "./lib/constants";
+import { MintStatusData } from "types/journal-types"
 
 /**
  * ログイン状態で、正しいスタンプラリーの合言葉が渡されていれば、
- * 記念のバッジNFTをmintする
+ * その合言葉にあったスタンプNFTをmintし、
+ * すべてのスタンプを集めた場合、記念バッジNFTをmintする。
+ * また、mintのtransaction発行に成功した場合、
+ * db上のmintStatusにおける、そのNFTのmint状態を"IN_PROGRESS"に設定する。
  */
 exports.checkReward = functions.region(REGION).https.onCall(async (data, context) => {
   // ユーザー認証周りのチェック
@@ -22,21 +24,60 @@ exports.checkReward = functions.region(REGION).https.onCall(async (data, context
   if (!email) {
     throw new functions.https.HttpsError("not-found", "The user doesn't have email.");
   }
-  // 初めてのリクエストであるかチェック
-  const stampRallyMintStatus: StampRallyMintStatusType = user?.stampRallyMintStatus;
-  if (stampRallyMintStatus === "IN_PROGRESS" || stampRallyMintStatus === "DONE") {
-    throw new functions.https.HttpsError("already-exists", "This request has already submitted.")
-  }
-  // 合言葉のチェック
-  if (data.keyword !== STAMP_RALLY_KEYWORD) {
+
+  // keyの型推論を含んだentries関数
+  const strictEntries = <T extends Record<string, any>>(
+    object: T
+  ): [keyof T, T[keyof T]][] => {
+    return Object.entries(object);
+  };
+  // 正しい合言葉のentryを探索・取得し、存在するかチェック
+  const correctStampEntry = strictEntries(TPF2023_STAMP_RALLY_KEYWORDS).find(v => v[1]===data.keyword);
+  if (!correctStampEntry) {
     throw new functions.https.HttpsError("invalid-argument", "The function must be called with correct keyword.")
   }
+
+  let isStampCompleted = false;
+  const mintStatusMap: MintStatusData = user?.mintStateTpf2023StampRally;
+  const tpf2023StampRallyMap = mintStatusMap?.tpf2023StampRally
+  if (tpf2023StampRallyMap) {
+    // 初めてのリクエストであるか（mint中・済みでないか）チェック
+    const mintStatus = tpf2023StampRallyMap[correctStampEntry[0]];
+    if (mintStatus === "IN_PROGRESS" || mintStatus === "DONE") {
+      throw new functions.https.HttpsError("already-exists", "This request has already submitted.")
+    }
+    // スタンプをコンプリートしたかチェック
+    isStampCompleted = (() => {
+      const requiredKeys = ["g0", "g1Alpha" ,"g1Beta", "g1Gamma", "g1Delta"]
+      // requiredKeysの要素を全てkeyとして持っているかを判定
+      const mapKeys = new Set(Object.keys(tpf2023StampRallyMap));
+      if (!requiredKeys.every(key => mapKeys.has(key))) {
+        return false;
+      }
+      // 全てのvalueがvalidValuesのどれかに当てはまっているかを判定
+      const validValues = new Set(["IN_PROGRESS", "DONE"]);
+      const mapValues = Object.values(tpf2023StampRallyMap);
+      if (!mapValues.every(value => validValues.has(value))) {
+        return false;
+      }
+      return true;
+    })();
+  }
+  
   // mint処理
 
-  // mint待機状態へ
-  await firestore().collection("users").doc(context.auth.uid).set({
-    stampRallyMintStatus: "IN_PROGRESS",
-  }, {
+  // スタンプをコンプリートしていたら追加でmint
+
+  // mint待機状態に設定
+  const setData: MintStatusData = {
+    tpf2023StampRally: {
+      [correctStampEntry[0]]: "IN_PROGRESS",
+    }
+  }
+  if (isStampCompleted) {
+    setData.tpf2023StampRally.complete = "IN_PROGRESS";
+  }
+  await firestore().collection("users").doc(context.auth.uid).set(setData, {
     merge: true,
   })
   return "正解！";
