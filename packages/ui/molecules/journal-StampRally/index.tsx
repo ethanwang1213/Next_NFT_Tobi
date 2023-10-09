f0510dcab77701fff9e22a0b9b53d59b38c994c1
ethanwang1213 <ethansage1213@gmail.com>
2023-10-09 07:17:29 +0000

    feat: update stamp img when mint is requested

--- packages/ui/molecules/journal-StampRally/index.tsx
+++ packages/ui/molecules/journal-StampRally/index.tsx
@@ -1,14 +1,36 @@
+import { useAuth } from "contexts/journal-AuthProvider";
 import { RoundedImage } from "../../atoms/journal-RoundedImage";
 import { StampRallyTitle } from "../../atoms/journal-StampRallyTitle";
 import { StampRallyRewardForm } from "../journal-StampRallyRewardForm";
 import stamp from "./assets/stamp.png";
+import { MintStatusType, Tpf2023StampType } from "types/journal-types";
+
+type StampDataType = {
+  key: Tpf2023StampType;
+  src: string;
+  status: MintStatusType;
+};
 
 /**
  * スタンプラリー特設表示のコンポーネント
  * @returns {ReactElement} The `StampRally` component
  */
 export const StampRally = () => {
-  const stamps = [stamp.src, stamp.src, stamp.src, stamp.src, stamp.src];
+  const { user } = useAuth();
+  const stampRally = user?.mintStatusData?.tpf2023StampRally;
+
+  const keys: Tpf2023StampType[] = [
+    "g0",
+    "g1Alpha",
+    "g1Beta",
+    "g1Gamma",
+    "g1Delta",
+  ];
+  const stamps: StampDataType[] = keys.map((key) => ({
+    key: key,
+    src: stamp.src,
+    status: !stampRally || !stampRally[key] ? "NOTHING" : stampRally[key],
+  }));
 
   return (
     <div className="text-center text-primary">
@@ -21,7 +43,15 @@ export const StampRally = () => {
         </p>
         <div className="mt-3 sm:mt-6 flex justify-center gap-3 sm:gap-6 h-[56px] sm:h-[104px]">
           {stamps.map((v) => (
-            <RoundedImage src={v} alt="fes stamp" width={105} height={105} />
+            <RoundedImage
+              key={v.key}
+              src={
+                v.status === "IN_PROGRESS" || v.status === "DONE" ? v.src : ""
+              }
+              alt={v.key}
+              width={105}
+              height={105}
+            />
           ))}
         </div>
       </div>
