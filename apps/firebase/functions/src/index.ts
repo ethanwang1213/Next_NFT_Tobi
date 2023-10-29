7ae4210520a86fa7cb27a47ac0fc6bfdbb426f78
ethanwang1213 <ethansage1213@gmail.com>
2023-10-29 05:58:36 +0000

    fix: remove debug api about stamp rally

--- apps/firebase/functions/src/index.ts
+++ apps/firebase/functions/src/index.ts
@@ -1,17 +1,17 @@
-import {initializeApp, applicationDefault} from "firebase-admin/app";
-import {firestore} from "firebase-admin";
-import {background} from "./shopifyApi";
-import {discordOAuth} from "./discord";
-import {discordUserNfts} from "./discordUserNfts";
-import {metadata} from "./tobiraneko";
-import {metadata as houseBadgeMetadata} from "./housebadge";
-import {mintFes23NftTask} from "./mintFes23NftTask";
+import { initializeApp, applicationDefault } from "firebase-admin/app";
+import { firestore } from "firebase-admin";
+import { background } from "./shopifyApi";
+import { discordOAuth } from "./discord";
+import { discordUserNfts } from "./discordUserNfts";
+import { metadata } from "./tobiraneko";
+import { metadata as houseBadgeMetadata } from "./housebadge";
+import { mintFes23NftTask } from "./mintFes23NftTask";
 
 // initializeApp() is not needed in Cloud Functions for Firebase
 initializeApp({
   credential: applicationDefault(),
 });
-firestore().settings({ignoreUndefinedProperties: true});
+firestore().settings({ ignoreUndefinedProperties: true });
 
 exports.shopifyOrders = require("./shopifyOrders");
 exports.journalNfts = require("./journalNfts");
@@ -21,9 +21,6 @@ exports.discordUserNfts = discordUserNfts;
 exports.tobiraneko = metadata;
 exports.housebadge = houseBadgeMetadata;
 exports.stampRallyBadge = require("./journalStampRallyBadge");
-// debug stamprally
-exports.stampRallyBadgeForDebug = require("./journalStampRallyBadgeForDebug");
-// end debug stamprally
 exports.mintFes23NftTask = mintFes23NftTask;
 
 if (process.env.PUBSUB_EMULATOR_HOST) {
