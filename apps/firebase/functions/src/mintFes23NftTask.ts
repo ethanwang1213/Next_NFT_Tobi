2beb6170d017bf263b79ad0fbadb86e2e4fd57a1
ethanwang1213 <ethansage1213@gmail.com>
2023-11-02 10:36:19 +0900

    fix: to lower case

--- apps/firebase/functions/src/mintFes23NftTask.ts
+++ apps/firebase/functions/src/mintFes23NftTask.ts
@@ -16,7 +16,7 @@ const mintNFT = async (name: string, description: string, userId: string, type: 
         return;
       }
       const nftId = totalSupply - 1;
-      const imageUrl = `${process.env.TOBIRAPOLIS_FESTIVAL23_BADGE_IMAGE_URL}${type}.png`;
+      const imageUrl = `${process.env.TOBIRAPOLIS_FESTIVAL23_BADGE_IMAGE_URL}${type.toLowerCase()}.png`;
       await createTobirapolisFestival23BadgeMetadata(nftId, type, name, description, imageUrl);
       await transferTobirapolisFestival23Badge(userId, nftId, name, description, new Date(), imageUrl, type);
 
