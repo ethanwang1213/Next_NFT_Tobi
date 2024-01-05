3bf68667c3bda3def4e115c9094604a2464be1ba
ethanwang1213 <ethansage1213@gmail.com>
2024-01-05 17:23:26 +0900

    feat: サインアウトの処理をcontextに含めた
    - サインアウトのページを作るよりも単純になるため

--- apps/admin/src/pages/signout.tsx
+++
@@ -1,9 +0,0 @@
-import useSignOut from "hooks/useSignOut";
-import SignOut from "ui/templates/SignOut";
-const Signout = () => {
-  useSignOut();
-
-  return <SignOut />;
-};
-
-export default Signout;
--- packages/contexts/AdminAuthProvider.tsx
+++ packages/contexts/AdminAuthProvider.tsx
@@ -1,5 +1,6 @@
 import { auth } from "fetchers/firebase/client";
 import { onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/router";
 import React, {
   ReactNode,
   createContext,
@@ -16,6 +17,7 @@ type Props = {
 // AuthContextのデータ型
 type ContextType = {
   user?: User;
+  signOut: () => Promise<void>;
 };
 
 const AuthContext = createContext<ContextType>({} as ContextType);
@@ -28,22 +30,15 @@ const AuthContext = createContext<ContextType>({} as ContextType);
 export const AuthProvider: React.FC<Props> = ({ children }) => {
   // ユーザー情報を格納するstate
   const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
   const MAX_NAME_LENGTH = 12;
-  // ユーザー作成用関数
-  function createUser(uid: string, email?: string | null) {
-    const appUser: User = {
-      id: uid,
-      name: email?.split("@")[0].slice(0, MAX_NAME_LENGTH) ?? "", // nameには、メールアドレスの@より前でMAX_NAME_LENGTH文字までを格納する
-      email: email ?? "",
-    };
-    setUser(appUser);
-  }
 
   useEffect(() => {
     // ログイン状態の変化を監視
     const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
       // ログイン状態の場合
       if (firebaseUser) {
+        // TODO: リリースする前に消そう
         console.log(`UID: ${firebaseUser.uid}`);
         console.log(`メールアドレス: ${firebaseUser.email}`);
         createUser(firebaseUser.uid, firebaseUser.email);
@@ -52,10 +47,32 @@ export const AuthProvider: React.FC<Props> = ({ children }) => {
     });
   }, []);
 
+  // ユーザー作成用関数
+  const createUser = (uid: string, email?: string | null) => {
+    const appUser: User = {
+      id: uid,
+      name: email?.split("@")[0].slice(0, MAX_NAME_LENGTH) ?? "", // nameには、メールアドレスの@より前でMAX_NAME_LENGTH文字までを格納する
+      email: email ?? "",
+    };
+    setUser(appUser);
+  };
+
+  const signOut = async () => {
+    try {
+      if (user?.email) {
+        await auth.signOut();
+      }
+      await router.push("/signin");
+    } catch (error) {
+      console.error("サインアウトに失敗しました。", error);
+    }
+  };
+
   return (
     <AuthContext.Provider
       value={{
         user,
+        signOut,
       }}
     >
       {children}
