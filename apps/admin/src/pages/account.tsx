0501f575485cc0f535387e24b01d4c04195e307e
Eduard <yojotony@outlook.com>
2024-01-31 03:12:03 +0200

    feat: add menu item pages

---
+++ apps/admin/src/pages/account.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "アカウントの設定",
+};
+
+export default function Index() {
+  return <div className="w-full">アカウントの設定ペジーです.</div>;
+}
