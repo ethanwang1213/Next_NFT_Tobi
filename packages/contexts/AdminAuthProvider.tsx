741dd6198274d39b87b1670e5ddb6dc9257ce5ab
ethanwang1213 <ethansage1213@gmail.com>
2024-02-18 17:48:47 +0900

    refactor: Changed to a more conventional style

--- packages/contexts/AdminAuthProvider.tsx
+++ packages/contexts/AdminAuthProvider.tsx
@@ -51,8 +51,8 @@ export const AuthProvider: React.FC<Props> = ({ children }) => {
         setUser(null);
         Router.push("/signin");
       }
-      return unsubscribe;
     });
+    return () => unsubscribe();
   }, []);
 
   // ユーザー作成用関数
