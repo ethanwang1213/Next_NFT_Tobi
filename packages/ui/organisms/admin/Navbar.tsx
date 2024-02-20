96ecc08b01926c81f379425080c105be4b42f8b0
ethanwang1213 <ethansage1213@gmail.com>
2024-02-20 16:47:41 +0900

    style: Adjusted the size of the hamburger button to match the sidebar icon.

--- packages/ui/organisms/admin/Navbar.tsx
+++ packages/ui/organisms/admin/Navbar.tsx
@@ -30,7 +30,7 @@ const NavbarStartBlock = () => {
     <>
       <button
         className={
-          "btn btn-block bg-non-active hover:bg-non-active border-0 w-[35px] min-h-[35px] h-[35px] ml-[18px] p-0"
+          "btn btn-block bg-non-active hover:bg-non-active border-0 w-[24px] min-h-[24px] h-[24px] ml-[18px] p-0"
         }
         onClick={onClickMenu}
         style={{
