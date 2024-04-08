597d737d66ca31290b2c277aebfac702b686519e
ethanwang1213 <ethansage1213@gmail.com>
2024-04-08 11:33:54 +0900

    refactor: Modified design.

--- apps/admin/src/pages/auth/password_reset.tsx
+++ apps/admin/src/pages/auth/password_reset.tsx
@@ -43,9 +43,7 @@ const PasswordReset = () => {
     }
   };
 
-  if (user?.registeredFlowAccount) {
-    return <div>redirect to top page</div>;
-  } else if (updatedPassword) {
+  if (updatedPassword) {
     return (
       <div className="flex flex-col items-center justify-center w-[100dvw] p-8">
         <Image
@@ -53,8 +51,9 @@ const PasswordReset = () => {
           alt={"Tobiratory logo"}
           width={110}
           height={114}
+          className={"mt-[200px]"}
         />
-        <div className={"mt-[200px]"}>パスワードをリセットしました</div>
+        <div className={"mt-[40px]"}>パスワードをリセットしました</div>
         <div>
           <button
             type={"button"}
