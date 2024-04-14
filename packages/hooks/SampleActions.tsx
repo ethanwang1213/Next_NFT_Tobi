a64f9cb9f5cfe0c52c334d249fa05523b199290d
Eduard <yojotony@outlook.com>
2024-04-14 15:44:39 +0300

    refactor: restore API

--- packages/hooks/SampleActions.tsx
+++ packages/hooks/SampleActions.tsx
@@ -1,5 +1,30 @@
 import { auth } from "fetchers/firebase/client";
 
+export const fetchSamples = async () => {
+  try {
+    const token = await auth.currentUser.getIdToken();
+    const response = await fetch(
+      "/backend/api/functions/native/admin/samples",
+      {
+        method: "GET",
+        headers: {
+          Authorization: token,
+        },
+      },
+    );
+    const result = await response.json();
+    if (result.status == "success") {
+      return result.data;
+    } else {
+      console.log("Error:", result.status);
+      return [];
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return [];
+  }
+};
+
 export const deleteSamples = async (ids) => {
   try {
     const token = await auth.currentUser.getIdToken();
