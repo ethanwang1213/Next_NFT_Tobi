5edb62d18afe2c485e47041fe8fe50f1e3b48206
Eduard <yojotony@outlook.com>
2024-04-26 09:26:47 +0300

    feat: implement draft of content brand page

--- packages/ui/organisms/admin/ContentBrandPanel.tsx
+++ packages/ui/organisms/admin/ContentBrandPanel.tsx
@@ -1,8 +1,26 @@
-import { useEffect, useState } from "react";
+import NextImage from "next/image";
+import { useEffect, useRef, useState } from "react";
+import ImageCropDialog from "./ImageCropDialog";
+
+const emptyImageSize = 240;
+const maxImageSize = 400;
 
 const ContentBrandPanel = () => {
-  // showcase data
-  const [showcases, setShowcases] = useState([]);
+  const [contentImageUrl, setContentImageUrl] = useState("");
+  const [contentImageSize, setContentImageSize] = useState({
+    w: emptyImageSize,
+    h: emptyImageSize,
+  });
+  const [stickerImageUrl, setStickerImageUrl] = useState("");
+  const [stickerImageSize, setStickerImageSize] = useState({
+    w: emptyImageSize,
+    h: emptyImageSize,
+  });
+  const [cropImageURL, setCropImageURL] = useState("");
+  const [cropImageFlag, setCropImageFlag] = useState(false);
+
+  const imageFileRef = useRef(null);
+  const imageCropDlgRef = useRef(null);
 
   // fetch showcases from server
   useEffect(() => {
@@ -19,11 +37,167 @@ const ContentBrandPanel = () => {
     fetchData();
   }, []);
 
+  const calcPreviewImageSize = (width, height) => {
+    if (width > maxImageSize || height > maxImageSize) {
+      if (width > height) {
+        return {
+          w: maxImageSize,
+          h: (maxImageSize * height) / width,
+        };
+      } else {
+        return {
+          w: (maxImageSize * width) / height,
+          h: maxImageSize,
+        };
+      }
+    } else {
+      return { w: width, h: height };
+    }
+  };
+
+  const handleFileInputChange = (event) => {
+    const file = event.target.files[0];
+    // Check if a file is selected
+    if (file) {
+      // Get the file extension
+      const fileExtension = file.name.split(".").pop().toLowerCase();
+      // Check if the file extension is 'png' or 'gif'
+      if (fileExtension === "png" || fileExtension === "gif") {
+        const imageUrl = URL.createObjectURL(file);
+        const img = new Image();
+        img.onload = function () {
+          onImageCropHandler(imageUrl, img.width, img.height);
+        };
+        img.src = imageUrl;
+      } else {
+        // File is not PNG or GIF, show an error message or handle accordingly
+        console.log("Please select a PNG or GIF file.");
+      }
+    }
+  };
+
+  const onImageCropHandler = (image, width, height) => {
+    if (cropImageFlag) {
+      setContentImageSize(calcPreviewImageSize(width, height));
+      setContentImageUrl(image);
+    } else {
+      setStickerImageSize(calcPreviewImageSize(width, height));
+      setStickerImageUrl(image);
+    }
+  };
+
   return (
-    <div className="flow-root">
-      <div className="inline-block min-w-full align-middle">
-        <div className="rounded-lg bg-gray-50 pt-0"></div>
+    <div className="max-w-[800px] min-w-[480px] flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <h2 className="text-secondary text-2xl font-bold">Content Image</h2>
+        <span className="text-neutral-400 text-xs font-medium py-2">
+          You can change the image of the content.
+        </span>
+        <div className="flex items-end gap-12">
+          <div
+            style={{ width: contentImageSize.w, height: contentImageSize.h }}
+            className={`
+              rounded-2xl border-2 border-primary-400
+              ${contentImageUrl ? "border-none" : "border-dashed"}`}
+          >
+            {contentImageUrl && (
+              <NextImage
+                src={contentImageUrl}
+                width={contentImageSize.w}
+                height={contentImageSize.h}
+                alt="Selected Image"
+                className={`rounded-2xl`}
+              />
+            )}
+          </div>
+          <button
+            className="text-xs font-medium text-primary"
+            onClick={() => {
+              setCropImageFlag(true);
+              if (!contentImageUrl) {
+                imageFileRef.current.click();
+              } else {
+                setCropImageURL(contentImageUrl);
+                imageCropDlgRef.current.showModal();
+              }
+            }}
+          >
+            {!contentImageUrl ? "upload" : "change"}
+          </button>
+          <button
+            className="text-xs font-medium text-primary"
+            onClick={() => {
+              setContentImageSize({ w: emptyImageSize, h: emptyImageSize });
+              setContentImageUrl("");
+            }}
+          >
+            delete
+          </button>
+        </div>
       </div>
+      <div className="flex flex-col gap-2">
+        <h2 className="text-secondary text-2xl font-bold">
+          Official Merchandise Sticker
+        </h2>
+        <span className="text-neutral-400 text-xs font-medium py-2">
+          For best results, use an image that is at least 500x500 pixels and no
+          more than 4MB in size. Accepted file formats are PNG or GIF
+          (non-animated).
+        </span>
+        <div className="flex items-end gap-12">
+          <div
+            style={{ width: stickerImageSize.w, height: stickerImageSize.h }}
+            className={`
+              rounded-2xl border-2 border-primary-400
+              ${stickerImageUrl ? "border-none" : "border-dashed"}`}
+          >
+            {stickerImageUrl && (
+              <NextImage
+                src={stickerImageUrl}
+                width={stickerImageSize.w}
+                height={stickerImageSize.h}
+                alt="Selected Image"
+                className={`rounded-2xl`}
+              />
+            )}
+          </div>
+          <button
+            className="text-xs font-medium text-primary"
+            onClick={() => {
+              setCropImageFlag(false);
+              if (!stickerImageUrl) {
+                imageFileRef.current.click();
+              } else {
+                setCropImageURL(stickerImageUrl);
+                imageCropDlgRef.current.showModal();
+              }
+            }}
+          >
+            {!stickerImageUrl ? "upload" : "change"}
+          </button>
+          <button
+            className="text-xs font-medium text-primary"
+            onClick={() => {
+              setStickerImageSize({ w: emptyImageSize, h: emptyImageSize });
+              setStickerImageUrl("");
+            }}
+          >
+            delete
+          </button>
+        </div>
+      </div>
+      <input
+        ref={imageFileRef}
+        type="file"
+        accept=".png, .gif"
+        onChange={(e) => handleFileInputChange(e)}
+        className="hidden"
+      />
+      <ImageCropDialog
+        initialValue={cropImageURL}
+        dialogRef={imageCropDlgRef}
+        changeHandler={onImageCropHandler}
+      />
     </div>
   );
 };
---
+++ packages/ui/organisms/admin/ImageCropDialog.tsx
@@ -0,0 +1,86 @@
+import "cropperjs/dist/cropper.css";
+import Image from "next/image";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
+import Cropper, { ReactCropperElement } from "react-cropper";
+
+const ImageCropDialog = ({
+  initialValue,
+  dialogRef,
+  changeHandler,
+}: {
+  initialValue: string;
+  dialogRef: MutableRefObject<HTMLDialogElement>;
+  changeHandler: (value: string, width: number, height: number) => void;
+}) => {
+  const [imageURL, setImageURL] = useState("");
+
+  const cropperRef = useRef<ReactCropperElement>(null);
+  const onCrop = () => null;
+
+  useEffect(() => {
+    setImageURL(initialValue);
+  }, [initialValue]);
+
+  return (
+    <dialog ref={dialogRef} className="modal">
+      <div className="modal-box max-w-[827px] rounded-3xl pt-4 flex flex-col gap-3 relative">
+        <form method="dialog">
+          <button className="absolute w-4 h-4 top-4 right-4">
+            <Image
+              src="/admin/images/icon/close2.svg"
+              width={16}
+              height={16}
+              alt="close icon"
+            />
+          </button>
+        </form>
+        <div className="text-base-black text-lg font-semibold">
+          Customize Image
+        </div>
+        <div className="w-full h-0 border-[0.5px] border-neutral-200"></div>
+        <Cropper
+          src={imageURL}
+          style={{ height: 400, width: "100%" }}
+          // Cropper.js options
+          initialAspectRatio={16 / 9}
+          guides={false}
+          crop={onCrop}
+          ref={cropperRef}
+        />
+        <div className="modal-action flex justify-end gap-4">
+          <button
+            type="button"
+            className="px-4 py-2 bg-primary rounded-[64px] 
+              text-base-white text-sm leading-4 font-semibold"
+            onClick={() => {
+              const cropper = cropperRef.current?.cropper;
+              // Get the cropped canvas
+              const croppedCanvas = cropper.getCroppedCanvas();
+              changeHandler(
+                croppedCanvas.toDataURL(),
+                croppedCanvas.width,
+                croppedCanvas.height,
+              );
+              dialogRef.current.close();
+            }}
+          >
+            Save changes
+          </button>
+          <button
+            type="button"
+            className="px-4 py-2 bg-base-white rounded-[64px] border-2 border-primary
+              text-primary text-sm leading-4 font-semibold"
+            onClick={() => dialogRef.current.close()}
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+      <form method="dialog" className="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+  );
+};
+
+export default ImageCropDialog;
