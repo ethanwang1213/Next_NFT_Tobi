461d58dbaa8d4fb96bdf55e3fd200757e7255a7f
dorrod-blip <supercoder0829@gmail.com>
2024-05-20 22:33:12 +0300

    feat: sample detail dialog

---
+++ packages/ui/organisms/admin/SampleDetailDialog.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import { MutableRefObject } from "react";
+
+const SampleDetailDialog = ({
+  thumbnail,
+  content,
+  item,
+  dialogRef,
+}: {
+  thumbnail: string;
+  content: string;
+  item: string;
+  dialogRef: MutableRefObject<HTMLDialogElement>;
+}) => {
+  return (
+    <dialog ref={dialogRef} className="modal">
+      <div className="modal-box max-w-[878px] rounded-3xl p-6 flex flex-col gap-3 justify-between items-center bg-opacity-90 bg-gray-800">
+        <form method="dialog">
+          <button className="absolute w-4 h-4 top-3 right-3">
+            <Image
+              src="/admin/images/icon/close2.svg"
+              width={16}
+              height={16}
+              alt="close icon"
+              style={{
+                filter:
+                  "opacity(0.7) brightness(0) saturate(100%) invert(100%)",
+              }}
+            />
+          </button>
+        </form>
+        <div className="mb-[56px]">
+          <Image
+            src="/admin/images/png/empty-image.png"
+            width={400}
+            height={400}
+            alt="Thumbnail Image"
+          />
+        </div>
+        <div className="flex justify-between items-center mb-4">
+          <span className="text-base-black text-base font-semibold text-gray-100 mr-8">
+            {content}
+          </span>
+          <span className="text-base-black text-2xl font-bold text-gray-100">
+            {item}
+          </span>
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default SampleDetailDialog;
