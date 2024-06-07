00cde1b7c1f893a424ea23dd95459a0841ea7f2c
dorrod-blip <supercoder0829@gmail.com>
2024-06-07 09:56:07 +0300

    chore: format file

--- packages/ui/organisms/admin/ShowcaseSampleTab.tsx
+++ packages/ui/organisms/admin/ShowcaseSampleTab.tsx
@@ -1,6 +1,6 @@
 import useRestfulAPI from "hooks/useRestfulAPI";
-import { useEffect, useState } from "react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { SampleItem } from "ui/types/adminTypes";
 
 const ShowcaseSampleTab = ({
