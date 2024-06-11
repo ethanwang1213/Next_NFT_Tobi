bc1001d581521da785792e293cc3b5c35516d94a
Eduard <globe.master.1215@outlook.com>
2024-06-11 16:46:55 +0300

    feat: configure components for acrylic stand sample creation

--- packages/ui/organisms/admin/WorkspaceSampleCreateDialog.tsx
+++ packages/ui/organisms/admin/WorkspaceSampleCreateDialog.tsx
@@ -1,5 +1,10 @@
 import NextImage from "next/image";
-import React, { MutableRefObject, useEffect, useState } from "react";
+import React, {
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import MaterialImageCropComponent from "ui/organisms/admin/MaterialImageCrop";
 import MaterialImageSelectComponent from "ui/organisms/admin/MaterialImageSelect";
 import RoadMapComponent from "ui/organisms/admin/SampleCreateRoadmap";
@@ -23,12 +28,144 @@ const WorkspaceSampleCreateDialog: React.FC<Props> = (props) => {
   const [creationStep, setCreationStep] = useState(0);
   const [sampleType, setSampleType] = useState(null);
   const [materialImage, setMaterialImage] = useState(null);
+  const [materialImage2, setMaterialImage2] = useState(null);
 
   useEffect(() => {
     setCreationStep(0);
     setSampleType(null);
     setMaterialImage(null);
+    setMaterialImage2(null);
   }, [props.initDialog]);
+
+  const placeComponent = useCallback(() => {
+    if (sampleType === null || creationStep === 0) {
+      return (
+        <SampleTypeSelectComponent
+          selectTypeHandler={(value) => {
+            setSampleType(value);
+            setCreationStep(1);
+          }}
+        />
+      );
+    }
+
+    switch (sampleType) {
+      case "Poster":
+        switch (creationStep) {
+          case 1:
+            return (
+              <MaterialImageSelectComponent
+                data={props.materials}
+                selectedImage={materialImage}
+                selectImageHandler={(value) => setMaterialImage(value)}
+                backHandler={() => setCreationStep(creationStep - 1)}
+                nextHandler={() => setCreationStep(creationStep + 1)}
+                uploadImageHandler={props.uploadImageHandler}
+              />
+            );
+
+          case 2:
+            return (
+              <MaterialImageCropComponent
+                materialImage={materialImage}
+                cropHandler={(image: string) =>
+                  setMaterialImage({ id: 0, image: image })
+                }
+                backHandler={() => setCreationStep(creationStep - 1)}
+                nextHandler={() => setCreationStep(creationStep + 1)}
+                generateHandler={props.generateHandler}
+                generateError={props.generateError}
+              />
+            );
+
+          default:
+            break;
+        }
+        break;
+
+      case "Acrylic Stand":
+        switch (creationStep) {
+          case 1:
+            return (
+              <MaterialImageSelectComponent
+                data={props.materials}
+                selectedImage={materialImage}
+                selectImageHandler={(value) => setMaterialImage(value)}
+                backHandler={() => setCreationStep(creationStep - 1)}
+                nextHandler={() => setCreationStep(creationStep + 1)}
+                uploadImageHandler={props.uploadImageHandler}
+              />
+            );
+
+          case 2:
+            return (
+              <MaterialImageCropComponent
+                materialImage={materialImage}
+                cropHandler={(image: string) =>
+                  setMaterialImage({ id: 0, image: image })
+                }
+                backHandler={() => setCreationStep(creationStep - 1)}
+                nextHandler={() => setCreationStep(creationStep + 1)}
+                generateHandler={props.generateHandler}
+                generateError={props.generateError}
+              />
+            );
+
+          case 3:
+            return (
+              <MaterialImageSelectComponent
+                data={props.materials}
+                selectedImage={materialImage2}
+                selectImageHandler={(value) => setMaterialImage2(value)}
+                backHandler={() => setCreationStep(creationStep - 1)}
+                nextHandler={() => setCreationStep(creationStep + 1)}
+                uploadImageHandler={props.uploadImageHandler}
+              />
+            );
+
+          case 4:
+            return (
+              <MaterialImageCropComponent
+                materialImage={materialImage2}
+                cropHandler={(image: string) =>
+                  setMaterialImage({ id: 0, image: image })
+                }
+                backHandler={() => setCreationStep(creationStep - 1)}
+                nextHandler={() => setCreationStep(creationStep + 1)}
+                generateHandler={props.generateHandler}
+                generateError={props.generateError}
+              />
+            );
+
+          case 5:
+            return (
+              <MaterialImageSelectComponent
+                data={props.materials}
+                selectedImage={materialImage}
+                selectImageHandler={(value) => setMaterialImage(value)}
+                backHandler={() => setCreationStep(creationStep - 1)}
+                nextHandler={() => setCreationStep(creationStep + 1)}
+                uploadImageHandler={props.uploadImageHandler}
+              />
+            );
+
+          default:
+            break;
+        }
+        break;
+
+      default:
+        break;
+    }
+  }, [
+    props,
+    sampleType,
+    creationStep,
+    materialImage,
+    materialImage2,
+    setSampleType,
+    setCreationStep,
+  ]);
 
   return (
     <dialog ref={props.dialogRef} className="modal">
@@ -47,36 +184,7 @@ const WorkspaceSampleCreateDialog: React.FC<Props> = (props) => {
           <RoadMapComponent sampleType={sampleType} step={creationStep} />
         </div>
         <div className="w-[400px] h-[492px] flex flex-col gap-4">
-          {creationStep === 0 && (
-            <SampleTypeSelectComponent
-              selectTypeHandler={(value) => {
-                setSampleType(value);
-                setCreationStep(1);
-              }}
-            />
-          )}
-          {creationStep === 1 && (
-            <MaterialImageSelectComponent
-              data={props.materials}
-              selectedImage={materialImage}
-              selectImageHandler={(value) => setMaterialImage(value)}
-              backHandler={() => setCreationStep(creationStep - 1)}
-              nextHandler={() => setCreationStep(creationStep + 1)}
-              uploadImageHandler={props.uploadImageHandler}
-            />
-          )}
-          {creationStep === 2 && sampleType === "Poster" && (
-            <MaterialImageCropComponent
-              materialImage={materialImage}
-              cropHandler={(image: string) =>
-                setMaterialImage({ id: 0, image: image })
-              }
-              backHandler={() => setCreationStep(creationStep - 1)}
-              nextHandler={() => setCreationStep(creationStep + 1)}
-              generateHandler={props.generateHandler}
-              generateError={props.generateError}
-            />
-          )}
+          {placeComponent()}
         </div>
       </div>
       <form method="dialog" className="modal-backdrop">
