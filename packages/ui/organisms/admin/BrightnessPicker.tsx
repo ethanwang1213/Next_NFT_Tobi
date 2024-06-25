24825c0fe99374f4351e6a313c0271f555b3672e
dorrod-blip <supercoder0829@gmail.com>
2024-06-25 09:34:05 +0300

    feat: FE & AI setting function

---
+++ packages/ui/organisms/admin/BrightnessPicker.tsx
@@ -0,0 +1,64 @@
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import React, { useState } from "react";
+
+interface BrightnessPickerProps {
+  initialValue?: number;
+  onBrightnessChanged: (newValue: number) => void;
+}
+
+const BrightnessPicker: React.FC<BrightnessPickerProps> = ({
+  initialValue = 50,
+  onBrightnessChanged,
+}) => {
+  const [brightness, setBrightness] = useState<number>(initialValue);
+  const handleBrightnessChange = (newValue: number) => {
+    setBrightness(newValue);
+    onBrightnessChanged(newValue);
+  };
+
+  const handleStyle = {
+    borderColor: "#FAFAFA",
+    height: 20,
+    width: 20,
+    marginLeft: 0,
+    backgroundColor: "#FAFAFA",
+  };
+
+  const trackStyle = {
+    backgroundColor: "#9F9C9C",
+    height: 8,
+  };
+
+  const railStyle = {
+    backgroundColor: "#9F9C9C",
+    height: 8,
+  };
+
+  return (
+    <div className="flex flex-col mt-6">
+      {/* Brightness text and value edit */}
+      <div className="flex items-center">
+        <div className="text-[16px] font-[700] leading-[20px] w-[76px]">
+          Brightness
+        </div>
+        <div className="w-[144px] h-[32px] rounded-[5px] bg-[#A5A1A1] text-white flex items-center justify-center font-[400] text-[16px] leading-[120%] mx-4">
+          {brightness}
+        </div>
+      </div>
+
+      {/* Brightness slider */}
+      <div className="flex items-center mt-[2px] mb-[26px]">
+        <Slider
+          min={0}
+          max={100}
+          styles={{ handle: handleStyle, track: trackStyle, rail: railStyle }}
+          value={brightness}
+          onChange={(value: number) => handleBrightnessChange(value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BrightnessPicker;
