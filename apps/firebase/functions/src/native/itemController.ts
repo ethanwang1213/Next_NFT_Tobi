f9e3fe9ef4feec5c0c6034784aa964f428787c48
ethanwang1213 <ethansage1213@gmail.com>
2024-07-03 11:39:57 +0300

    fix: add more params to get sample API

--- apps/firebase/functions/src/native/itemController.ts
+++ apps/firebase/functions/src/native/itemController.ts
@@ -1126,6 +1126,9 @@ export const getSampleInfo = async (req: Request, res: Response) => {
         id: sampleData.id,
         title: sampleData.digital_item.name,
         description: sampleData.digital_item.description,
+        modelType: sampleData.digital_item.type,
+        modelUrl: sampleData.model_url,
+        imageUrl: sampleData.digital_item.is_default_thumb?sampleData.digital_item.default_thumb_url:sampleData.digital_item.custom_thumb_url,
         creator: {
           uuid: sampleData.user.uuid,
           username: sampleData.user.username,
