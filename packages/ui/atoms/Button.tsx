e2b4d33ed0d52b7dfbd90b9ff08e9e285644d045
Eduard <globe.master.1215@outlook.com>
2024-07-10 19:36:38 +0300

    style: fix floating button issue

--- packages/ui/atoms/Button.tsx
+++ packages/ui/atoms/Button.tsx
@@ -11,9 +11,10 @@ type Props = DetailedHTMLProps<
 const Button = ({ className, children, ...props }: Props) => {
   return (
     <button
-      className={`relative enabled:hover:shadow-xl enabled:hover:-top-[3px] transition-shadow ${
-        className ?? ""
-      }`}
+      className={`relative enabled:hover:shadow-xl enabled:hover:-top-[3px] transition-shadow 
+        enabled:hover:after:absolute enabled:hover:after:h-[5px] enabled:hover:after:-bottom-[5px] 
+        enabled:hover:after:left-0 enabled:hover:after:right-0 
+        ${className ?? ""}`}
       {...props}
     >
       {children}
