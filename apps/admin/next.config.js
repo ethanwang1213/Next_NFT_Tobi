52c05a10406da6dbba07b2b45a803a1f4d90de23
ethanwang1213 <ethansage1213@gmail.com>
2024-10-26 16:52:25 +0900

    Revert "try: modify rewrites rules"
    This reverts commit 7d0807c8c725ec6ce6d5352d04a3a8794a3eace5.

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -35,8 +35,12 @@ module.exports = {
   async rewrites() {
     return [
       {
+        source: '/:locale(en|jp)/:path*',
+        destination: `/admin/:path*`,
+      },
+      {
         basePath: false,
-        source: "/:locale(en|jp)/proxy/:path*",
+        source: "/proxy/:path*",
         destination:
           // "http://127.0.0.1:7777/v0/b/tobiratory-f6ae1.appspot.com/o/:path*",
           // "https://firebasestorage.googleapis.com/v0/b/tobiratory.appspot.com/o/:path*",
@@ -45,15 +49,11 @@ module.exports = {
       },
       {
         basePath: false,
-        source: "/:locale(en|jp)/backend/api/functions/:path*",
+        source: "/backend/api/functions/:path*",
         destination:
           // "http://localhost:5001/tobiratory-f6ae1/asia-northeast1/:path*",
           // "https://asia-northeast1-tobiratory-f6ae1.cloudfunctions.net/:path*",
           `${process.env.CLOUD_FUNCTIONS_API_URL}/:path*`,
-      },
-      {
-        source: '/:locale(en|jp)/:path*',
-        destination: `/admin/:path*`,
       },
     ];
   },
