0b3ab976617dfad4ec49052512b7f3749a948e00
ethanwang1213 <ethansage1213@gmail.com>
2024-12-07 09:46:29 +0100

    fix : "Edit Item Data” button hit

--- packages/ui/organisms/admin/WorkspaceSampleDetailPanel.tsx
+++ packages/ui/organisms/admin/WorkspaceSampleDetailPanel.tsx
@@ -18,7 +18,7 @@ const WorkspaceSampleDetailPanel: React.FC<WorkspaceSampleDetailPanelProps> = ({
   deleteAllActionHistory,
 }) => {
   return (
-    <div className="absolute top-0 left-0 lg:w-[316px] w-[280px] h-full bg-[#001327] lg:px-[28px] px-[15px] pt-6 pb-4 pointer-events-auto">
+    <div className="absolute top-0 left-0 lg:w-[316px] w-[280px] h-full bg-[#001327] lg:px-[28px] px-[15px] pt-6 pb-4 pointer-events-auto z-20">
       <div className="h-full flex flex-col justify-center">
         <SampleDetailView
           id={id}
