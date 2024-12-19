5a020fc0d46f989a9960ad66844a6ff74cd476de
ethanwang1213 <ethansage1213@gmail.com>
2024-12-19 13:04:20 +0100

    fix : cms approval

--- apps/admin/src/pages/apply/contentApproval.tsx
+++ apps/admin/src/pages/apply/contentApproval.tsx
@@ -1,6 +1,10 @@
+import { auth, db } from "fetchers/firebase/client";
+import { doc, getDoc } from "firebase/firestore/lite";
 import { GetStaticPropsContext } from "next";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
   return {
@@ -12,6 +16,40 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
 
 const ContentApproval = () => {
   const t = useTranslations("ContentReview");
+  const router = useRouter();
+  const [accountData, setAccountData] = useState(null);
+
+  useEffect(() => {
+    let interval;
+
+    const fetchAccountData = async () => {
+      if (auth.currentUser && auth.currentUser.uid) {
+        const { uid } = auth.currentUser;
+        const accountRef = doc(db, `businessAccount/${uid}`);
+
+        try {
+          const snapshot = await getDoc(accountRef);
+          if (snapshot.exists()) {
+            const data = snapshot.data();
+            if (data.cmsApprove) {
+              router.push("/items");
+            }
+          } else {
+            console.log("No account data found for this user.");
+          }
+        } catch (error) {
+          console.error("Error fetching document:", error);
+        }
+      }
+    };
+
+    fetchAccountData();
+
+    interval = setInterval(fetchAccountData, 5000);
+
+    return () => clearInterval(interval);
+  }, [auth.currentUser]);
+
   return (
     <div className="mt-10 px-20">
       <div className="py-9 px-8 bg-primary mx-auto rounded-2xl font-sans">
