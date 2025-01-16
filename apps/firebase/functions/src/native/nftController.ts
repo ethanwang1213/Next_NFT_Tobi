68a5e9806a7d77f4192b185952eedbfc255b88ca
ethanwang1213 <ethansage1213@gmail.com>
2025-01-16 22:31:45 +0900

    feat: call fetchNFTOwnershipHistory API

--- apps/firebase/functions/src/native/nftController.ts
+++ apps/firebase/functions/src/native/nftController.ts
@@ -1,8 +1,9 @@
 import {Request, Response} from "express";
-import {DecodedIdToken, getAuth} from "firebase-admin/auth";
 import {FirebaseError, storage} from "firebase-admin";
+import {DecodedIdToken, getAuth} from "firebase-admin/auth";
+import {getFunctions, httpsCallable} from "firebase/functions";
 import {v4 as uuidv4} from "uuid";
-import {TOBIRATORY_DIGITAL_ITEMS_ADDRESS, TOPIC_NAMES} from "../lib/constants";
+import {TOPIC_NAMES} from "../lib/constants";
 import {PubSub} from "@google-cloud/pubsub";
 import {pushToDevice} from "../appSendPushMessage";
 import {prisma} from "../prisma";
@@ -15,14 +16,10 @@ import {
   notificationBatchStatus,
   digitalItemStatus,
 } from "./utils";
-import * as fcl from "@onflow/fcl";
-
-fcl.config({
-  "flow.network": process.env.FLOW_NETWORK ?? "FLOW_NETWORK",
-  "accessNode.api": process.env.FLOW_ACCESS_NODE_API ?? "FLOW_ACCESS_NODE_API",
-});
 
 const pubsub = new PubSub();
+const functions = getFunctions();
+const fetchNFTOwnershipHistory = httpsCallable(functions, "fetchNFTOwnershipHistory");
 
 export const mintNFT = async (req: Request, res: Response) => {
   const {id} = req.params;
@@ -502,46 +499,9 @@ export const generateNotificationBatchId = async (fcmToken: string) => {
 };
 
 export const getOwnershipHistory = async (ownerFlowAddress: string, nftId: number) => {
-  const cadence = `
-import TobiratoryDigitalItems from 0x${TOBIRATORY_DIGITAL_ITEMS_ADDRESS}
-
-access(all) fun main(address: Address, id: String): {UFix64: Address}? {
-    let collection = getAccount(address)
-        .capabilities.get<&TobiratoryDigitalItems.Collection>(TobiratoryDigitalItems.CollectionPublicPath)
-        .borrow()
-        ?? panic("NFT Collection not found")
-
-    let nft = collection.borrowTobiratoryNFT(UInt64.fromString(id)!)
-
-    return nft?.getOwnerHistory();
-}`;
-
-  try {
-    console.log(`Fetch ownership history: ${ownerFlowAddress}, ${nftId}`);
-    console.log(` flow.network: ${process.env.FLOW_NETWORK}, accessNode.api: ${process.env.FLOW_ACCESS_NODE_API}`);
-    console.log(JSON.stringify(await fcl.config().all()));
-
-    fcl.send([fcl.ping()])
-        .then(() => {
-          console.log("SUCCESS");
-        })
-        .catch((e) => {
-          console.log("ERROR: ");
-          throw e;
-        });
-
-    const result = await fcl.query({
-      cadence,
-      args: (arg: any, t: any) => [arg(ownerFlowAddress, t.Address), arg(String(nftId), t.String)],
-    });
-    console.log(`Result: ${JSON.parse(result)}`);
-    return result;
-  } catch (e) {
-    console.log("The error occurs while fetching ownership history: ");
-    console.error(e);
-  }
-
-  return;
+  const result = await fetchNFTOwnershipHistory({ account: ownerFlowAddress, id: nftId });
+  console.log(`Ownership history has been fetched: ${result.data}`);  
+  return JSON.parse(String(result.data));
 };
 
 export const fetchNftThumb = async (req: Request, res: Response) => {
