d3bb8685e65968f95980aaa904474d6faed8c0ec
ethanwang1213 <ethansage1213@gmail.com>
2025-03-21 18:20:50 +0900

    fix: Fixed error handling for creating flow account.

--- packages/fetchers/adminUserAccount.ts
+++ packages/fetchers/adminUserAccount.ts
@@ -1,3 +1,4 @@
+import { isFlowAccountProcessing } from "contexts/AdminAuthProvider";
 import { auth } from "fetchers/firebase/client";
 import { useLocale, useTranslations } from "next-intl";
 import { useState } from "react";
@@ -39,26 +40,30 @@ export const useTobiratoryAndFlowAccountRegistration = () => {
           // If Flow account is already registered, the account data is returned.
           setResponse(resData.data);
         } else {
-          // Wait until the Flow account is created.
-          const maxRetry = 100;
-          const sleepTime = 10000; // 10 seconds
-          for (let i = 0; i < maxRetry; i++) {
-            await new Promise((resolve) => setTimeout(resolve, sleepTime));
-            const profile = await fetchMyProfile();
-            if (profile.data?.flow?.flowAddress) {
-              setResponse(profile.data);
-              setLoading(false);
+          const waitResult = await waitFlowAccountCreation();
+          if (waitResult) {
+            setResponse(waitResult);
+          } else {
+            setError(t("LogInSignUp.FailedMessageToCreateFlowAccount"));
+          }
+        }
+      } else {
+        // The response may not always be in JSON format.
+        const resData = await res.text();
+        console.error(resData);
+        try {
+          const data = JSON.parse(resData);
+          if (isFlowAccountProcessing(data.data)) {
+            const waitResult = await waitFlowAccountCreation();
+            if (waitResult) {
+              setResponse(waitResult);
               return;
-            } else if (profile.data?.type === FlowAccountStatus.Error) {
-              break;
             }
           }
           setError(t("LogInSignUp.FailedMessageToCreateFlowAccount"));
+        } catch {
+          setError(t("Error.Retry"));
         }
-      } else {
-        const resData = await res.text();
-        console.error(resData);
-        setError(t("Error.Retry"));
       }
     } catch (error) {
       console.error(String(error));
@@ -72,13 +77,17 @@ export const useTobiratoryAndFlowAccountRegistration = () => {
 
 const registerToTobiratoryAndFlowAccount = async (locale: string) => {
   const idToken = await auth.currentUser.getIdToken(true);
-  await fetch(`/backend/api/functions/native/signup`, {
+  const res = await fetch(`/backend/api/functions/native/signup`, {
     method: "POST",
     headers: {
       Authorization: idToken,
       "Content-Type": "application/json",
     },
   });
+
+  if (!res.ok) {
+    return res;
+  }
 
   const data = { locale };
   return await fetch(`/backend/api/functions/native/create-flow`, {
@@ -89,4 +98,19 @@ const registerToTobiratoryAndFlowAccount = async (locale: string) => {
     },
     body: JSON.stringify(data),
   });
+};
+
+const waitFlowAccountCreation = async () => {
+  const maxRetry = 100;
+  const sleepTime = 10000; // 10 seconds
+  for (let i = 0; i < maxRetry; i++) {
+    await new Promise((resolve) => setTimeout(resolve, sleepTime));
+    const profile = await fetchMyProfile();
+    if (profile.data?.flow?.flowAddress) {
+      return profile.data;
+    } else if (profile.data?.type === FlowAccountStatus.Error) {
+      return false;
+    }
+  }
+  return false;
 };
